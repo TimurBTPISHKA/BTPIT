3311de20f67c4826224236c746cf7365bad7d3da
Timur <tfattahov4@gmail.com>
2022-11-21 14:46:21 +0300

    БТПИТ

---
+++ app/src/main/java/com/example/pract3/ObViewModel.java
@@ -0,0 +1,7 @@
+package com.example.pract3;
+
+import androidx.lifecycle.ViewModel;
+
+public class ObViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/pract3/ob.java
@@ -0,0 +1,36 @@
+package com.example.pract3;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ob extends Fragment {
+
+    private ObViewModel mViewModel;
+
+    public static ob newInstance() {
+        return new ob();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ob, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(ObViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
